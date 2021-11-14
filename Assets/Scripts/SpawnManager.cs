ebef1424b6413522b233c0c3945b7838587c35c7
Tahir Ovayurt <iovayurt@gmail.com>
2021-11-14 19:53:26 +0300

    Audio

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,6 +8,9 @@ public class SpawnManager : MonoBehaviour
     private GameObject _enemyPrefab;
     [SerializeField]
     private GameObject _enemyContainer;
+
+    private bool _stopSpawning = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,12 +25,17 @@ public class SpawnManager : MonoBehaviour
 
     IEnumerator SpawnRoutine()
     {
-      while (true)
+      while (_stopSpawning == false)
       {
           Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
           GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
           newEnemy.transform.parent = _enemyContainer.transform;
           yield return new WaitForSeconds(4.0f);
       }
+    }
+
+    public void OnPlayerDeath()
+    {
+      _stopSpawning = true;
     }
 }
