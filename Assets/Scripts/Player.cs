638fb837e398083a2296ab79b2124e2b3148ed82
Tahir Ovayurt <iovayurt@gmail.com>
2021-11-18 19:08:18 +0300

    game over

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -117,6 +117,8 @@ public class Player : MonoBehaviour
 
         _lives--;
 
+        _uiManager.UpdateLives(_lives);
+
         if (_lives < 1)
         {
           _spawnManager.OnPlayerDeath();
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,16 +7,33 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField]
     private Text _scoreText;
+    [SerializeField]
+    private Image _LivesImg;
+    [SerializeField]
+    private Sprite[] _liveSprites;
+    [SerializeField]
+    private Text _gameOverText;
 
     // Start is called before the first frame update
     void Start()
     {
       _scoreText.text = "Score: " + 0;
+      _gameOverText.gameObject.SetActive(false);
     }
 
     public void UpdateScore(int playerScore)
     {
       _scoreText.text = "Score: " + playerScore.ToString();
+    }
+
+    public void UpdateLives(int currentLives)
+    {
+      _LivesImg.sprite = _liveSprites[currentLives];
+
+      if (currentLives == 0)
+      {
+        _gameOverText.gameObject.SetActive(true);
+      }
     }
 
 }
