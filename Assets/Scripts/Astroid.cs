7d1b3df8e1f22c000d323422801163ec34f91db5
Tahir Ovayurt <iovayurt@gmail.com>
2021-11-19 23:59:07 +0300

    explosion

--- Assets/Scripts/Astroid.cs
+++ Assets/Scripts/Astroid.cs
@@ -6,16 +6,22 @@ public class Astroid : MonoBehaviour
 {
     [SerializeField]
     private float _rotateSpeed = 20.0f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    [SerializeField]
+    private GameObject _explosionPrefab;
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Laser")
+        {
+          Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+          Destroy(this.gameObject, 0.25f);
+          Destroy(other.gameObject);
+        }
     }
 }
