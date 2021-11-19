674e445654202263474576713ae868d38dd9257c
Tahir Ovayurt <iovayurt@gmail.com>
2021-11-19 16:39:09 +0300

    Main Menu

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -13,12 +13,22 @@ public class UIManager : MonoBehaviour
     private Sprite[] _liveSprites;
     [SerializeField]
     private Text _gameOverText;
+    [SerializeField]
+    private Text _restartText;
+
+    private GameManager _gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
       _scoreText.text = "Score: " + 0;
       _gameOverText.gameObject.SetActive(false);
+      _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
+
+      if (_gameManager == null)
+      {
+        Debug.LogError("GameManager is NULL.");
+      }
     }
 
     public void UpdateScore(int playerScore)
@@ -32,7 +42,26 @@ public class UIManager : MonoBehaviour
 
       if (currentLives == 0)
       {
-        _gameOverText.gameObject.SetActive(true);
+        GameOverSequence();
+      }
+    }
+
+    void GameOverSequence()
+    {
+      _gameManager.GameOver();
+      _gameOverText.gameObject.SetActive(true);
+      _restartText.gameObject.SetActive(true);
+      StartCoroutine(GameOverFlickerRoutine());
+    }
+
+    IEnumerator GameOverFlickerRoutine()
+    {
+      while(true)
+      {
+        _gameOverText.text = "GAME OVER";
+        yield return new WaitForSeconds(0.5f);
+        _gameOverText.text = "";
+        yield return new WaitForSeconds(0.5f);
       }
     }
 
